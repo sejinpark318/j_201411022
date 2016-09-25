60842aa8e5bdef44a9d39debd078c1197e59a823
박세진 j1_201411022 <sejinpark318@naver.com>
2016-09-25 16:22:37 +0900

    class BrazilCoffee

---
+++ 2-3.java
@@ -0,0 +1,20 @@
+class BrazilCoffee{
+  String origin;
+  int degreeOfRoast;
+  int grade;
+  BrazilCoffee(String origin) {
+    this.origin=origin;
+  }
+  public void roast(){
+  System.out.println("roastin..."+origin);
+  }
+  public void grind(){
+  System.out.println("grinding..."+origin);
+  }
+  public void brew(){
+  System.out.println("brewing..."+origin);
+  }
+}
+BrazilCoffee brazil=new BrazilCoffee("Brazil");
+brazil.roast()
+->roastin...Brazil
