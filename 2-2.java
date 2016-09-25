0cec72594b912d2536dfe112e9a25b5c8f427ee5
박세진 j1_201411022 <sejinpark318@naver.com>
2016-09-25 16:19:00 +0900

    Add files via upload

---
+++ 2-2.java
@@ -0,0 +1,14 @@
+class Subway{
+  int line;
+  void run(){
+    System.out.println(line+" lines run");
+  }
+  void setLine(int l){
+    line=1;
+  }
+}
+
+Subway mySub = new Subway()
+
+mySub.setLine(5)
+mySub.run()
