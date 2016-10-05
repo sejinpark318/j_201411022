8febc8b97b9aa24773c09ebc551ad103c8057022
박세진 j1_201411022 <sejinpark318@naver.com>
2016-10-05 16:28:20 +0900

    Add files via upload

---
+++ DiceGame.java
@@ -0,0 +1,20 @@
+class DiceGame{
+  private Player p1;
+  private Player p2;
+  public void startGame(){
+    p1=new Player("A");
+    p2=new Player("B");
+    p1.play();
+    p2.play();
+    if(p1.getNum()>p1.getNum()){
+      System.out.println(p1.getName() +" won");
+    }
+    else if (p1.getNum()==p2.getNum()){
+      System.out.println("draw");
+    }
+    else{
+      System.out.println(p2.getName()+" won");
+    }
+  }
+}
+
