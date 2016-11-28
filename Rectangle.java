1c52a40f37a170ca4e0ffa1af8a15716274e4396
박세진 j1_201411022 <sejinpark318@naver.com>
2016-11-28 10:31:35 +0900

    ch8

--- Rectangle.java
+++ Rectangle.java
@@ -1,10 +1,22 @@
-﻿class Rectangle{
-  private int width;
-  private int height;
-  public Rectangle(int w, int h){
-    this.width=w;
-    this.height=h;
-  }
-  public int calcArea(){
-    return width*height;
+package com.sd.ch8;
+public class Rectangle{
+    protected double length;
+    protected double width;
+    protected boolean isSelected;
+    public Rectangle(double l, double w){
+        length=l;
+        width=w;
+    }
+    public double getLength(){
+        return length;
+    }
+    public double getWidth(){
+        return width;
+    }
+    public void setSelected(boolean b){
+        isSelected=b;
+    }
+    public boolean getSelected(){
+        return isSelected;
+    }
 }
