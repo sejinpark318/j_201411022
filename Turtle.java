88ac2664757693f2d265173249a8f03696767ba5
박세진 j1_201411022 <sejinpark318@naver.com>
2016-12-05 12:31:51 +0900

    ch8_2

---
+++ Turtle.java
@@ -0,0 +1,22 @@
+package com.sd.ch8;
+
+public class Turtle{
+    public int nLimbs = 4;
+    protected String food=null;
+    
+    public void setFood(String f){
+        this.food = f;
+    }
+    public void eat(){
+        System.out.println("Turtle eats "+food);
+    }
+    public static void main(String[] args){
+        Turtle t = new Turtle();
+        t.setFood("seaweed");
+        t.eat();
+        SeaTurtle st = new SeaTurtle();
+        st.swim();
+        LandTurtle lt = new LandTurtle();
+        lt.walk();
+    }
+}
