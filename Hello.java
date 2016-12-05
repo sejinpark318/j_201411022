1ccf127d4cd962d1680ba9ace2bc666e8fa7a309
박세진 j1_201411022 <sejinpark318@naver.com>
2016-12-05 12:34:08 +0900

    ch8_5

--- Hello.java
+++ Hello.java
@@ -1,7 +1,21 @@
-class Hello{
-  String toWhom="world";
-  Hello(){}
-  void sayHello(){
-  System.out.println("hello"+toWhom);
-  }
+package com.sd.ch8;
+
+public class Hello{
+    private String name;
+    public Hello(String name){
+        this.name=name;
+    }
+    public void sayHello(){
+        System.out.println("Hello "+name);
+    }
+    public static void main(String[] args){
+        Hello[] h = new Hello[5];
+        h[0] = new Hello("psj1");
+        h[1] = new Hello("psj2");
+        h[2] = new Hello("psj3");
+        h[3] = new Hello("psj4");
+        h[4] = new Hello("psj5");
+        for(Hello e:h)
+            e.sayHello();
+    }
 }
