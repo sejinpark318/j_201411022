47da7e5ade726053e5e8c49065d9ef883c6be1f1
박세진 j1_201411022 <sejinpark318@naver.com>
2016-12-05 12:32:06 +0900

    ch8_2

---
+++ SeaTurtle.java
@@ -0,0 +1,9 @@
+package com.sd.ch8;
+//import com.sd.ch8.Turtle;
+
+public class SeaTurtle extends Turtle{
+    public void swim(){
+        System.out.print("A sea turtle swims with"+nLimbs+"legs");
+    }
+
+}
