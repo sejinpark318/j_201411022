3a40f7714c659211a91cd220392d804364559724
박세진 j1_201411022 <sejinpark318@naver.com>
2016-12-05 12:32:20 +0900

    ch8_2

---
+++ LandTurtle.java
@@ -0,0 +1,10 @@
+package com.sd.ch8;
+//import com.sd.ch8.Turtle;
+
+public class LandTurtle extends Turtle{
+    
+    public void walk(){
+        System.out.print("A Land turtle walks with"+nLimbs+ "legs");
+    }
+   
+}
